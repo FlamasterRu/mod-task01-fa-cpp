8249f139aafb1457b22b173f1f17ef0185e615ec
Nikita <nikita172000@yandex.ru>
2021-03-11 10:38:36 +0300

    complited

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,12 +1,78 @@
-unsigned int faStr1(const char *str)
+unsigned int faStr1(const char* str)
 {
-    return 0;
+    unsigned int result = 0;
+    for (int i = 0; str[i] != '\0'; ++i)
+    {
+        if (str[i] == ' ')
+        {
+            continue;
+        }
+        bool noDigit = true;
+        while ((str[i] != ' ') && (str[i] != '\0'))
+        {
+            if ((str[i] >= '0') && (str[i] <= '9'))
+            {
+                noDigit = false;
+            }
+            ++i;
+        }
+        if (noDigit)
+        {
+            ++result;
+        }
+    }
+    return result;
 }
-unsigned int faStr2(const char *str)
+unsigned int faStr2(const char* str)
 {
-    return 0;
+    unsigned int result = 0;
+    for (int i = 0; str[i] != '\0'; ++i)
+    {
+        if (str[i] == ' ')
+        {
+            continue;
+        }
+        bool flag = false;
+        if ((str[i] >= 'A') && (str[i] <= 'Z'))
+        {
+            flag = true;
+            ++i;
+            while ((str[i] != ' ') && (str[i] != '\0'))
+            {
+                if ((str[i] < 'a') || (str[i] > 'z'))
+                {
+                    flag = false;
+                }
+                ++i;
+            }
+        }
+        if (flag)
+        {
+            ++result;
+        }
+    }
+    return result;
 }
-unsigned int faStr3(const char *str)
+unsigned int faStr3(const char* str)
 {
-    return 0;
+    unsigned int result = 0;
+    double sum = 0;
+    double word = 0;
+    for (int i = 0; str[i] != '\0'; ++i)
+    {
+        if (str[i] == ' ')
+        {
+            continue;
+        }
+        ++word;
+        int count = 0;
+        while ((str[i] != ' ') && (str[i] != '\0'))
+        {
+            ++count;
+            ++i;
+        }
+        sum += count;
+    }
+    double r = sum / word;
+    return round(r);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,9 @@
 
 int main()
 {
+	const char* str = " aaa1  Aaaaa    aaa 1111  a. ";
+	unsigned result1 = faStr1(str);
+	unsigned result2 = faStr2(str);
+	unsigned result3 = faStr3(str);
     return 0;
 }
